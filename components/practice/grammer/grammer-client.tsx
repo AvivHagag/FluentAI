00bd728ea73dda150262fa4d84c54bd7a2de0977
tomere13 <98044440+tomere13@users.noreply.github.com>
2024-07-02 23:31:55 +0300

    check jenkins3

--- components/practice/grammer/grammer-client.tsx
+++ components/practice/grammer/grammer-client.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { Button } from '../../ui/button'
-//import { Button } fro'@/coponents/ui/button'
+//import { Button } fro'@/coponents/ui/bun'
 //import { generatePrompt } from '@/actions/openai'
 import { generatePrompt } from '../../../actions/openai'
 import { useState } from 'react'

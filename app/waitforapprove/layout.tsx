6916434dcd0cac7b87c88780a6273d9eda93ef75
AvivHagag <Aviv1049@gmail.com>
2024-07-19 14:11:46 +0300

    bug fix waitforapprove page

--- app/waitforapprove/layout.tsx
+++ app/waitforapprove/layout.tsx
@@ -2,8 +2,8 @@ import LogoTitle from "@/components/auth/LogoTitle";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full flex flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,var(--tw-gradient-stops))] from-lightBeige to-mediumBeige">
-      <div className="-mt-16">
+    <div className="h-screen pt-16 flex flex-col items-center bg-[radial-gradient(ellipse_at_top,var(--tw-gradient-stops))] from-lightBeige to-mediumBeige">
+      <div className="">
         <LogoTitle />
       </div>
       {children}

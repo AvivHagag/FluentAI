16c20664955d921530d5826ea4970af0c688e205
AvivHagag <Aviv1049@gmail.com>
2024-07-26 00:21:11 +0300

    finish, bug fix

--- app/student/page.tsx
+++ app/student/page.tsx
@@ -10,13 +10,15 @@ const StudentPage = async () => {
       <div>
         {session?.user.name && <WelcomePage name={session?.user.name} />}
       </div>
-      <h2 className="text-center text-xl md:text-2xl lg:text-3xl 2xl:text-5xl text-darkRed mb-1 sm:mb-2">
-        סטטיסטיקות
-      </h2>
       {session?.user.id && (
-        <div className="flex flex-row space-x-3 md:space-x-2 sm:px-2 md:px-4 w-4/5 mx-auto">
-          <StudentStatistics id={session?.user.id} />
-        </div>
+        <>
+          <h2 className="text-center text-xl md:text-2xl lg:text-3xl 2xl:text-5xl text-darkRed mb-1 sm:mb-2">
+            סטטיסטיקות
+          </h2>
+          <div className="flex flex-row space-x-3 md:space-x-2 sm:px-2 md:px-4 w-4/5 mx-auto">
+            <StudentStatistics id={session?.user.id} />
+          </div>
+        </>
       )}
       <StudentOptionsCards />
     </>

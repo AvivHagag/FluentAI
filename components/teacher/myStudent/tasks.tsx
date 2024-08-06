57f3b99586bf183450c56c7cf8b96802f6b6b620
AvivHagag <Aviv1049@gmail.com>
2024-08-06 17:32:12 +0300

    finish status task and create content

--- components/teacher/myStudent/tasks.tsx
+++ components/teacher/myStudent/tasks.tsx
@@ -9,58 +9,15 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { getStudentTasks } from "@/lib/ServerActions/ServerActions";
 import Link from "next/link";
-
-const invoices = [
-  {
-    invoice: "INV001",
-    paymentStatus: "Paid",
-    totalAmount: "$250.00",
-    paymentMethod: "Credit Card",
-  },
-  {
-    invoice: "INV002",
-    paymentStatus: "Pending",
-    totalAmount: "$150.00",
-    paymentMethod: "PayPal",
-  },
-  {
-    invoice: "INV003",
-    paymentStatus: "Unpaid",
-    totalAmount: "$350.00",
-    paymentMethod: "Bank Transfer",
-  },
-  {
-    invoice: "INV004",
-    paymentStatus: "Paid",
-    totalAmount: "$450.00",
-    paymentMethod: "Credit Card",
-  },
-  {
-    invoice: "INV005",
-    paymentStatus: "Paid",
-    totalAmount: "$550.00",
-    paymentMethod: "PayPal",
-  },
-  {
-    invoice: "INV006",
-    paymentStatus: "Pending",
-    totalAmount: "$200.00",
-    paymentMethod: "Bank Transfer",
-  },
-  {
-    invoice: "INV007",
-    paymentStatus: "Unpaid",
-    totalAmount: "$300.00",
-    paymentMethod: "Credit Card",
-  },
-];
 
 interface TasksProps {
   studentId: string;
 }
 
-export function Tasks({ studentId }: TasksProps) {
+export async function Tasks({ studentId }: TasksProps) {
+  const StudentData = await getStudentTasks(studentId);
   return (
     <>
       <div className="mx-auto my-2">
@@ -73,38 +30,51 @@ export function Tasks({ studentId }: TasksProps) {
           </Button>
         </Link>
       </div>
-
-      <div className="flex flex-col justify-center w-2/3 mx-auto bg-white bg-opacity-20 border border-white border-opacity-30 rounded-lg shadow-xl backdrop-blur">
-        <Table>
-          <TableCaption>A list of your recent invoices.</TableCaption>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-[100px]">Invoice</TableHead>
-              <TableHead>Status</TableHead>
-              <TableHead>Method</TableHead>
-              <TableHead className="text-right">Amount</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {invoices.map((invoice) => (
-              <TableRow key={invoice.invoice}>
-                <TableCell className="font-medium">{invoice.invoice}</TableCell>
-                <TableCell>{invoice.paymentStatus}</TableCell>
-                <TableCell>{invoice.paymentMethod}</TableCell>
-                <TableCell className="text-right">
-                  {invoice.totalAmount}
-                </TableCell>
+      {StudentData && (
+        <div className="flex flex-col justify-center w-2/3 mx-auto bg-white bg-opacity-20 border border-white border-opacity-30 rounded-lg shadow-xl backdrop-blur">
+          <Table dir="rtl">
+            <TableCaption>המטלות האחרונות של {StudentData.name}</TableCaption>
+            <TableHeader dir="rtl">
+              <TableRow dir="rtl">
+                <TableHead className="text-right font-medium text-darkRed">
+                  מספר
+                </TableHead>
+                <TableHead className="text-right font-medium text-darkRed">
+                  שם המטלה
+                </TableHead>
+                <TableHead className="text-right font-medium text-darkRed">
+                  להגשה עד
+                </TableHead>
+                <TableHead className="text-right font-medium text-darkRed">
+                  הודעה
+                </TableHead>
+                <TableHead className="text-right font-medium text-darkRed">
+                  ציון
+                </TableHead>
+                <TableHead className="text-right font-medium text-darkRed">
+                  סטטוס
+                </TableHead>
               </TableRow>
-            ))}
-          </TableBody>
-          <TableFooter>
-            <TableRow>
-              <TableCell colSpan={3}>Total</TableCell>
-              <TableCell className="text-right">$2,500.00</TableCell>
-            </TableRow>
-          </TableFooter>
-        </Table>
-      </div>
+            </TableHeader>
+            <TableBody dir="rtl">
+              {StudentData.tasks.map((task, index) => (
+                <TableRow key={task.id}>
+                  <TableCell className="font-medium">{index + 1}</TableCell>
+                  <TableCell className="font-medium">{task.id}</TableCell>
+                  <TableCell>
+                    {new Date(task.date).toLocaleDateString()}
+                  </TableCell>
+                  <TableCell>{task.messageText}</TableCell>
+                  <TableCell>{task.grade ?? " - "}</TableCell>
+                  <TableCell>
+                    {task.approvedByAdmin ? "מאושר" : "ממתין לאישור מנהל האתר"}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+      )}
     </>
   );
 }
--- lib/openai.ts
+++ lib/openai.ts
@@ -33,6 +33,13 @@ export const vocabularyRequest = async (LevelChosen: string) => {
   return Result;
 };
 
+export const vocabularyRequestSingle = async (LevelChosen: string) => {
+  console.log("start");
+  const prompt = `Please provide one English word at the ${LevelChosen} difficulty level in JSON format. The JSON object should contain two arrays: 'words' for the single English word and 'answers' for the single Hebrew translations.`;
+  const Result = await generatePrompt(prompt);
+  return Result;
+};
+
 export const getHint = async (
   textForHint: string,
   answerForHint: string | null,

aee7563b6a3c5ef8932a8549d55649a771bf936d
AvivHagag <Aviv1049@gmail.com>
2024-08-17 18:53:18 +0300

    fix loader

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,13 +1,11 @@
 import React from "react";
-import HashLoader from "react-spinners/HashLoader";
 
 export default function Loading() {
   return (
     <div className="flex flex-col justify-center items-center min-h-screen">
-      <p className="text-darkRed text-xxs py-2" dir="rtl">
+      <p className="text-darkRed text-base py-2" dir="rtl">
         טוען ..
       </p>
-      <HashLoader color="#E85A4F" size={40} />
     </div>
   );
 }
